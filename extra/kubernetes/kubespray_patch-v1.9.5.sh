5c3e0808be169d3c7476f80674a7ee67ecb84e0b
slpcat <slpcat@qq.com>
2018-04-16 15:36:04 +0800

    kubespracy change

---
+++ extra/kubernetes/kubespray_patch-v1.9.5.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#change to private registry 
+# current dir contains "kubespray"
+PRIVATE_KUBE_REGISTRY="slpcat"
+PRIVATE_QUAYIO_REGISTRY="slpcat"
+gcr_image_files=(
+./kubespray/roles/kubernetes-apps/ansible/defaults/main.yml
+./kubespray/roles/download/defaults/main.yml
+)
+
+for file in ${gcr_image_files[@]} ; do
+    sed -i "s/gcr.io\/google_containers/${PRIVATE_KUBE_REGISTRY}/g" $file
+done
+
+#quay_image_files=(
+#./kubespray/roles/download/defaults/main.yml
+#./kubespray/roles/etcd/tasks/install_rkt.yml
+#./kubespray/roles/kubernetes/node/tasks/install_rkt.yml
+#./kubespray/roles/kubernetes/node/defaults/main.yml
+#)
+
+#for file in ${quay_image_files[@]} ; do
+#    sed -i "s/quay.io\/coreos\//${PRIVATE_QUAYIO_REGISTRY}\/coreos-/g" $file
+#    sed -i "s/quay.io\/calico\//${PRIVATE_QUAYIO_REGISTRY}\/calico-/g" $file
+#    sed -i "s/quay.io\/l23network\//${PRIVATE_QUAYIO_REGISTRY}\/l23network-/g" $file
+#done
+sed -i 's/gcr.io\/kubernetes-helm/slpcat/' ./kubespray/roles/download/defaults/main.yml
+#disable docker install
+sed -i '/role:\ docker/d' ./kubespray/cluster.yml 
+
+#change congfig
+sed -i '/ndots/ndots:\ 5/' ./kubespray/inventory/local/group_vars/k8s-cluster.yml
+sed -i '/efk_enabled/efk_enabled:\ true/' ./kubespray/inventory/local/group_vars/k8s-cluster.yml 
+sed -i '/helm_enabled/helm_enabled:\ true/' ./kubespray/inventory/local/group_vars/k8s-cluster.yml
+sed -i '/stio_enabled/istio_enabled:\ true/' ./kubespray/inventory/local/group_vars/k8s-cluster.yml
+sed -i '/local_volume_provisioner_enabled/local_volume_provisioner_enabled:\ true/' ./kubespray/inventory/local/group_vars/k8s-cluster.yml
+sed -i '/ngress_nginx_enabled/ingress_nginx_enabled:\ true/' ./kubespray/inventory/local/group_vars/k8s-cluster.yml
+sed -i '/ubeconfig_localhost/kubeconfig_localhost:\ true/' ./kubespray/inventory/local/group_vars/k8s-cluster.yml
+
