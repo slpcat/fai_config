6738cb6bca9fd14d6bad9a960dfcec1c0fd9b4fb
root <root@>
2018-05-25 00:47:05 +0800

    kube change

--- extra/kubernetes/kubespray_patch-v1.10.2.sh
+++ extra/kubernetes/kubespray_patch-v1.10.2.sh
@@ -53,17 +53,20 @@ sed -i '/role:\ docker/d' ./kubespray/cluster.yml
 #              gather_timeout: 90
 
 #change cluster congfig
-sed -i 's/^ndots.*$/ndots:\ 5/' ./kubespray/inventory/local/group_vars/k8s-cluster.yml
-sed -i 's/^kube_proxy_mode.*$/kube_proxy_mode:\ ipvs/' ./kubespray/inventory/local/group_vars/k8s-cluster.yml
-sed -i 's/^efk_enabled.*$/efk_enabled:\ true/' ./kubespray/inventory/local/group_vars/k8s-cluster.yml 
-#sed -i 's/^helm_enabled.*$/helm_enabled:\ true/' ./kubespray/inventory/local/group_vars/k8s-cluster.yml
-#sed -i 's/^istio_enabled.*$/istio_enabled:\ true/' ./kubespray/inventory/local/group_vars/k8s-cluster.yml
-sed -i 's/^kube_version.*$/kube_version:\ v1.10.2/' ./kubespray/inventory/local/group_vars/k8s-cluster.yml
-sed -i 's/^local_volume_provisioner_enabled.*$/local_volume_provisioner_enabled:\ true/' ./kubespray/inventory/local/group_vars/k8s-cluster.yml
-sed -i 's/^ingress_nginx_enabled.*$/ingress_nginx_enabled:\ true/' ./kubespray/inventory/local/group_vars/k8s-cluster.yml
-sed -i 's/^kubeconfig_localhost.*$/kubeconfig_localhost:\ true/' ./kubespray/inventory/local/group_vars/k8s-cluster.yml
-#sed -i 's/^#\ kubelet_enforce_node_allocatable.*$/kubelet_enforce_node_allocatable:\ kube-reserved/' ./kubespray/inventory/local/group_vars/k8s-cluster.yml
-#sed -i 's/^#kube_token_auth.*$/kube_token_auth:\ true/' ./kubespray/inventory/local/group_vars/k8s-cluster.yml
+sed -i 's/^ndots.*$/ndots:\ 5/' ./kubespray/inventory/sample/group_vars/k8s-cluster.yml
+sed -i 's/^kube_proxy_mode.*$/kube_proxy_mode:\ ipvs/' ./kubespray/inventory/sample/group_vars/k8s-cluster.yml
+sed -i 's/^efk_enabled.*$/efk_enabled:\ true/' ./kubespray/inventory/sample/group_vars/k8s-cluster.yml 
+#sed -i 's/^helm_enabled.*$/helm_enabled:\ true/' ./kubespray/inventory/sample/group_vars/k8s-cluster.yml
+#sed -i 's/^istio_enabled.*$/istio_enabled:\ true/' ./kubespray/inventory/sample/group_vars/k8s-cluster.yml
+sed -i 's/^dns_mode.*$/dns_mode:\ coredns_dual/' ./kubespray/inventory/sample/group_vars/k8s-cluster.yml
+sed -i 's/^kube_version.*$/kube_version:\ v1.10.2/' ./kubespray/inventory/sample/group_vars/k8s-cluster.yml
+sed -i 's/^local_volume_provisioner_enabled.*$/local_volume_provisioner_enabled:\ true/' ./kubespray/inventory/sample/group_vars/k8s-cluster.yml
+sed -i 's/^ingress_nginx_enabled.*$/ingress_nginx_enabled:\ true/' ./kubespray/inventory/sample/group_vars/k8s-cluster.yml
+sed -i 's/^#\ ingress_nginx_host_network.*$/ingress_nginx_host_network:\ true/' ./kubespray/inventory/sample/group_vars/k8s-cluster.yml
+sed -i 's/^kubeconfig_localhost.*$/kubeconfig_localhost:\ true/' ./kubespray/inventory/sample/group_vars/k8s-cluster.yml
+#sed -i 's/^#\ kubelet_enforce_node_allocatable.*$/kubelet_enforce_node_allocatable:\ kube-reserved/' ./kubespray/inventory/sample/group_vars/k8s-cluster.yml
+#sed -i 's/^#kube_token_auth.*$/kube_token_auth:\ true/' ./kubespray/inventory/sample/group_vars/k8s-cluster.yml
+sed -i 's/^#\ kubeconfig_localhost.*$/kubeconfig_localhost:\ true/' ./kubespray/inventory/sample/group_vars/k8s-cluster.yml
 
 sed -i 's/^#kube_read_only_port.*$/kube_read_only_port:\ 10255/' ./kubespray/inventory/sample/group_vars/all.yml
 sed -i 's/^#kubelet_load_modules.*$/kubelet_load_modules:\ true/' ./kubespray/inventory/sample/group_vars/all.yml
@@ -76,15 +79,11 @@ sed -i 's/^etcd_events_cluster_setup.*$/etcd_events_cluster_setup:\ true/' ./kub
 #sed -i /kibana_base_url/d  ./kubespray/roles/kubernetes-apps/efk/kibana/templates/kibana-deployment.yml.j2
 #sed -i /KIBANA_BASE_URL/d  ./kubespray/roles/kubernetes-apps/efk/kibana/templates/kibana-deployment.yml.j2
 
-#change ingress-nginx config
-#sed -i '/annotations:$/a\nsecurity.alpha.kubernetes.io/sysctls: net.ipv4.tcp_syncookies=0,net.ipv4.ip_local_port_range=10000 65535/' roles/kubernetes-apps/ingress_controller/ingress_nginx/templates/ingress-nginx-controller-ds.yml.j2
-#security.alpha.kubernetes.io/unsafe-sysctls: net.core.somaxconn=65535,net.ipv4.tcp_tw_reuse=1,net.ipv4.tcp_fin_timeout=30,net.ipv4.tcp_keepalive_intvl=4,net.ipv4.tcp_keepalive_probes=3,net.ipv4.tcp_keepalive_time=120,net.ipv4.tcp_max_syn_backlog=65535,net.ipv4.tcp_rfc1337=1,net.ipv4.tcp_slow_start_after_idle=0,net.ipv4.tcp_fack=1,net.ipv4.tcp_fwmark_accept=1,net.ipv4.fwmark_reflect=1
-
 #change local-volume-provisioner
 echo 'reclaimPolicy: Retain' >> ./kubespray/roles/kubernetes-apps/external_provisioner/local_volume_provisioner/templates/local-volume-provisioner-sc.yml.j2
 
 #change etcd config
-#sed -i 's/^etcd_extra_vars.*$/etcd_extra_vars:\n   - ETCD_MAX_REQUEST_BYTES: "32M"\n  - ETCD_QUOTA_BACKEND_BYTES: "16G"/' ./kubespray/roles/etcd/defaults/main.yml
+#sed -i 's/^etcd_extra_vars.*$/etcd_extra_vars:\n   - ETCD_MAX_REQUEST_BYTES: "32M"\n  - ETCD_QUOTA_BACKEND_BYTES: "8G"/' ./kubespray/roles/etcd/defaults/main.yml
 
 #Azure cloudprovider
 #inventory/sample/group_vars/all.yml
