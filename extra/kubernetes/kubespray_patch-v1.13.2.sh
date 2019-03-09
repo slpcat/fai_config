9b62f3d1dea01afd7faad02a797c8e3ac7c35842
slpcat <slpcat@qq.com>
2019-03-10 01:12:30 +0800

    kube

--- extra/kubernetes/kubespray_patch-v1.13.2.sh
+++ extra/kubernetes/kubespray_patch-v1.13.2.sh
@@ -102,6 +102,8 @@ sed -i 's/^cilium_memory_limit.*$/cilium_memory_limit:\ 2Gi/' ./kubespray/roles/
 sed -i 's/^cilium_cpu_limit.*$/cilium_cpu_limit:\ 2/' ./kubespray/roles/network_plugin/cilium/defaults/main.yml
 
 sed -i 's/^kube_controller_pod_eviction_timeout.*$/kube_controller_pod_eviction_timeout:\ 1m0s/' ./kubespray/roles/kubernetes/master/defaults/main/main.yml
+#kubernetes dashboard
+sed -i 's/^dashboard_token_ttl.*$/dashboard_token_ttl:\ 86400/' ./roles/kubernetes-apps/ansible/defaults/main.yml
 
 #change local-volume-provisioner
 #echo 'reclaimPolicy: Retain' >> ./kubespray/roles/kubernetes-apps/external_provisioner/local_volume_provisioner/templates/local-volume-provisioner-sc.yml.j2
